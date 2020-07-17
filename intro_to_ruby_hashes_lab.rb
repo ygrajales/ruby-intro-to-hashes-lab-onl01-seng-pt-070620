c26c5d314ea90d87d13aca1302a6fa54e42b5638
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-17 01:56:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,8 @@ def monopoly_with_third_tier
 monopoly = {
 	  :railroads => {
 	    :pieces => 4,
-	    :rent_in_dollars => {},
-	    :names => {}
+	    :names => {},
+	    :rent_in_dollars => {}
 	  }
 	}
 
